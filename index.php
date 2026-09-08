4d638661bb9e7cc595589289091f73dbb11b3117
Adriangnr <adriancaslagnr@gmail.com>
2026-09-07 22:37:17 -0300

    debug

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
  * page-contacto.php y single-trabajo.php para todo el contenido real.
  */
 
+ ini_set('display_errors', 1);
+ ini_set('display_startup_errors', 1);
+ error_reporting(E_ALL);
+
 if (!defined('ABSPATH')) {
     exit;
 }
